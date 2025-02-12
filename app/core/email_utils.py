cad3990cc3c6a2de37be7701f50226b4d3b05fe8
Alvaro Guarachi <alvaro.guarachi.ticona@gmail.com>
2025-02-12 19:40:19 -0400

    fix: send email

--- app/core/email_utils.py
+++ app/core/email_utils.py
@@ -7,35 +7,28 @@ from fastapi import HTTPException
 
 def send_email(to_email: str, subject: str, body: str):
     try:
-        # Cargar el template HTML
         template_path = Path(__file__).parent / "templates" / "email_template.html"
         with open(template_path, "r", encoding="utf-8") as file:
             html_template = file.read()
-
-        # Reemplazar el código en el template
         html_content = html_template.replace("{{ code }}", body)
 
-        # Conectar al servidor SMTP
         server = SMTP(settings.EMAIL_SERVER, settings.EMAIL_PORT)
         if settings.EMAIL_TLS:
             server.starttls()
 
-        # Codificar credenciales en utf-8
         username = settings.EMAIL_USERNAME.encode('utf-8').decode('utf-8')
         password = settings.EMAIL_PASSWORD.encode('utf-8').decode('utf-8')
 
-        # Autenticación
         server.login(username, password)
-        # Crear el mensaje de correo
         msg = MIMEMultipart()
         msg['From'] = settings.EMAIL_FROM
         msg['To'] = to_email
         msg['Subject'] = subject
-
-        # Adjuntar el contenido HTML
         msg.attach(MIMEText(html_content, 'html'))
-        # Enviar el correo
+
         server.sendmail(settings.EMAIL_FROM, to_email, msg.as_string())
         server.quit()
     except Exception as e:
+        # Aquí puedes registrar el error en un log o imprimirlo
+        print("Error al enviar el correo:", e)
         raise HTTPException(status_code=500, detail=f"Error al enviar el correo: {str(e)}")
--- app/core/security.py
+++ app/core/security.py
@@ -17,19 +17,6 @@ def create_access_token(user_id: int, contact_id: int, expires_delta: timedelta 
     to_encode.update({"exp": expire})
     return jwt.encode(to_encode, settings.JWT_SECRET_KEY, algorithm=settings.JWT_ALGORITHM)
 
-# def verify_token(token: str = Depends(oauth2_scheme)):
-#     try:
-#         if token in blacklisted_tokens:
-#             raise HTTPException(status_code=401, detail="Token inválido o expirado")
-#         payload = jwt.decode(token, settings.JWT_SECRET_KEY, algorithms=[settings.JWT_ALGORITHM])
-#         user_id = payload.get("user_id")
-#         contact_id = payload.get("contact_id")
-#         if user_id is None or contact_id is None:
-#             raise HTTPException(status_code=401, detail="Token inválido: falta información.")
-#         return payload  # Retorna el payload completo
-#     except JWTError:
-#         raise HTTPException(status_code=401, detail="Token inválido o expirado")
-
 def create_password_reset_token(user_id: int, expires_minutes: int = 15) -> str:
     """
     Crea un token JWT para el restablecimiento de contraseña con un tiempo de expiración corto.
