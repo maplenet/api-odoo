6a06f71cb55ca1cd9cbee5b2811788f91204fc2d
Alvaro Guarachi <alvaro.guarachi.ticona@gmail.com>
2025-03-13 18:39:51 -0400

    refactor: reorganize database query functions and implement user query logic

--- query_db.py
+++ query_db.py
@@ -2,41 +2,13 @@ import sqlite3
 
 # registro de codigos
 
-def query_verification():
-    # Conectar a la base de datos (se asume que verification.db está en el mismo directorio)
-    connection = sqlite3.connect("verification.db")
-    cursor = connection.cursor()
-
-    # Ejecutar la consulta para obtener todos los registros de la tabla verification
-    cursor.execute("SELECT * FROM verification")
-    rows = cursor.fetchall()
-
-    # Cerrar la conexión
-    connection.close()
-
-    # Mostrar los resultados
-    if rows:
-        print("Registros en la tabla 'verification':")
-        for row in rows:
-            print(row)
-    else:
-        print("No se encontraron registros en la tabla 'verification'.")
-
-if __name__ == '__main__':
-    query_verification()
-
-# -----------------------------------------------------------        
-
-
-# registro de usuarios
-
-# def query_users():
+# def query_verification():
 #     # Conectar a la base de datos (se asume que verification.db está en el mismo directorio)
 #     connection = sqlite3.connect("verification.db")
 #     cursor = connection.cursor()
 
-#     # Ejecutar la consulta para obtener todos los registros de la tabla users
-#     cursor.execute("SELECT * FROM users")
+#     # Ejecutar la consulta para obtener todos los registros de la tabla verification
+#     cursor.execute("SELECT * FROM verification")
 #     rows = cursor.fetchall()
 
 #     # Cerrar la conexión
@@ -44,14 +16,42 @@ if __name__ == '__main__':
 
 #     # Mostrar los resultados
 #     if rows:
-#         print("Registros en la tabla 'users':")
+#         print("Registros en la tabla 'verification':")
 #         for row in rows:
 #             print(row)
 #     else:
-#         print("No se encontraron registros en la tabla 'users'.")
+#         print("No se encontraron registros en la tabla 'verification'.")
 
 # if __name__ == '__main__':
-#     query_users()
+#     query_verification()
+
+# -----------------------------------------------------------        
+
+
+# registro de usuarios
+
+def query_users():
+    # Conectar a la base de datos (se asume que verification.db está en el mismo directorio)
+    connection = sqlite3.connect("verification.db")
+    cursor = connection.cursor()
+
+    # Ejecutar la consulta para obtener todos los registros de la tabla users
+    cursor.execute("SELECT * FROM users")
+    rows = cursor.fetchall()
+
+    # Cerrar la conexión
+    connection.close()
+
+    # Mostrar los resultados
+    if rows:
+        print("Registros en la tabla 'users':")
+        for row in rows:
+            print(row)
+    else:
+        print("No se encontraron registros en la tabla 'users'.")
+
+if __name__ == '__main__':
+    query_users()
 
 # -----------------------------------------------------------        
 
