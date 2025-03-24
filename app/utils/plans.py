89cad51cab73b47b8e6592f1a80f7c7d8189bf4a
Alvaro Guarachi <alvaro.guarachi.ticona@gmail.com>
2025-03-24 11:03:24 -0400

    Uncomment product IDs for production in plans.py

--- app/utils/plans.py
+++ app/utils/plans.py
@@ -1,21 +1,21 @@
 # Lista de ids de productos en odoo test
-PRODUCTS = {
-    "ID_ESTADAR": 11,
-    "PREMIUM_PLUS": 13,
-    "HBO": 14,
-    "ID_PREMIUM": 22,
-    "[M+M] FUTBOL": 12, # TODO: BORRAR
-}
+# PRODUCTS = {
+#     "ID_ESTADAR": 11,
+#     "PREMIUM_PLUS": 13,
+#     "HBO": 14,
+#     "ID_PREMIUM": 22,
+#     "[M+M] FUTBOL": 12, # TODO: BORRAR
+# }
 
 # Lista de ids de productos en odoo prod
 
-# PRODUCTS = {
-#     "M+_ESTANDAR_PROMO": 6,
-#     "M+_ESTANDAR": 7,
-#     "M+ _REMIUM": 8,
-#     "M+_PREMIUM_+_HBO": 9,
-#     "BRASIL_VS_COLOMBIA_20-03-2025": 46,
-#     "ECUADOR_VS_VENEZUELA_21-03-2025": 47,
-#     "URUGUAY VS. ARGENTINA_21-03-2025": 48,
-#     "BOLIVIA_VS_URUGUAY_25-03-2025": 49,
-# }
+PRODUCTS = {
+    "M+_ESTANDAR_PROMO": 6,
+    "M+_ESTANDAR": 7,
+    "M+ _REMIUM": 8,
+    "M+_PREMIUM_+_HBO": 9,
+    "BRASIL_VS_COLOMBIA_20-03-2025": 46,
+    "ECUADOR_VS_VENEZUELA_21-03-2025": 47,
+    "URUGUAY VS. ARGENTINA_21-03-2025": 48,
+    "BOLIVIA_VS_URUGUAY_25-03-2025": 49,
+}
